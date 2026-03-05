0b317ae4e630c84eb80ade57d9860931acb5d2fa
Lesie Fernandes <lesie0223@gmail.com>
2026-03-05 09:58:41 +0530

    Delete ad-hoc request - 2.sql

--- ad-hoc request - 2.sql
+++
@@ -1,8 +0,0 @@
--- 2. Generate a report that provides an overview of the number of stores in each city.
-
-select
-city,
-count(store_id) as total_stores
-from dim_stores
-group by city
-order by count(store_id) desc;
